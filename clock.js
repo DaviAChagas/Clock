8bb7fcc15bd8d590381c2f067b5e180e8cb39856
Davi Almeida Chagas <davi.a.chagas@unesp.br>
2021-05-31 12:37:03 -0300

    Relógio muda de cor dependendo da cor: 06-17 e 18-05

--- clock.js
+++ clock.js
@@ -2,7 +2,39 @@ const clockContainer = document.querySelector('.mother-container')
 
 const formatTimeUnit = unit => String(unit).length === 1 ? `0${unit}` : unit
 
+
+const formatColor = hours => {
+
+    if((06 >= formatTimeUnit(hours)) || ((formatTimeUnit(hours) < 18))){
+        return "#369FFF, #006CE6"
+    }
+    if((05 <= formatTimeUnit(hours)) || ((formatTimeUnit(hours) >= 18))){
+        return "#7C838A, #E6F3FF"
+}
+}
+
+
+const getColorSpan = (hours) => `
+
+<style>
+
+@media only screen and (max-width: 700px) and (min-width: 374px){
+ .clock-container span, .date-container span{ 
+     padding: clamp(4px, 1.4vw, 2rem);
+     background: linear-gradient(to bottom right, ${formatColor(hours)});
+    }}
+
+@media only screen and (min-width: 701px) {
+ .clock-container span {
+ background: linear-gradient(to bottom right, ${formatColor(hours)});
+}}
+
+</style>
+`
+
    const getClockHTML = (daysNames,days,years, months, dates, hours, minutes, seconds) => `
+
+   ${getColorSpan(hours)}
 
 <div class="dayName-container"><span>${daysNames[days]}</span></div>
 
@@ -40,7 +72,6 @@ const dates = present.getDate()
 const hours = present.getHours()
 const minutes = present.getMinutes()
 const seconds = present.getSeconds()
-
 
 clockContainer.innerHTML = getClockHTML(daysNames,days,years, months, dates, hours, minutes, seconds)
 }
